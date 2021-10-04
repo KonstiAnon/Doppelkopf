9467c92f3d5add0b0e4b9b02590748360a90d151
Julius Kreutz <jk@julius-kreutz.de>
2021-10-04 16:59:45 +0200

    Added Round

--- src/main/java/de/konsti/doppelkopfspring/model/Round.java
+++ src/main/java/de/konsti/doppelkopfspring/model/Round.java
@@ -1,4 +1,17 @@
 package de.konsti.doppelkopfspring.model;
 
 public class Round {
+
+    private final boolean solo;
+    private final boolean bock;
+    private final int points;
+    private final int player1, player2;
+
+    public Round(boolean solo, boolean bock, int points, int player1, int player2) {
+        this.solo = solo;
+        this.bock = bock;
+        this.points = points;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 }
