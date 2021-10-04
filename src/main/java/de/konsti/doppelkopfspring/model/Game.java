ebbd410d0d94b9ac2fb2806a0812afe5b408ed45
Konstantin Kissel <konstantin.kissel@tum.de>
2021-10-04 16:51:40 +0200

    Added Linked List for Rounds

--- src/main/java/de/konsti/doppelkopfspring/model/Game.java
+++ src/main/java/de/konsti/doppelkopfspring/model/Game.java
@@ -1,15 +1,19 @@
 package de.konsti.doppelkopfspring.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Game {
 
     private final boolean fivePlayer;
     private final Player[] players;
+    private final LinkedList<Round> rounds;
 
     public Game(boolean fivePlayer, String... names) {
         this.players = Arrays.stream(names).limit(fivePlayer ? 5 : 4).map(Player::new).toArray(Player[]::new);
         this.fivePlayer = fivePlayer;
+        this.rounds = new LinkedList<>();
     }
 
     public Player[] getPlayers() {
---
+++ src/main/java/de/konsti/doppelkopfspring/model/Round.java
@@ -0,0 +1,4 @@
+package de.konsti.doppelkopfspring.model;
+
+public class Round {
+}
